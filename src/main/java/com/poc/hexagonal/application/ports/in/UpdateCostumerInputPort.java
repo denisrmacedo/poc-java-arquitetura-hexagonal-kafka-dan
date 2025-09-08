f670039fb429167dfdf476f8ce29a6323ce7fde6
Denis Macedo <denisrmacedo@gmail.com>
2025-09-08 14:26:16 -0300

    Implementar o consumer do Kafka

--- src/main/java/com/poc/hexagonal/application/ports/in/UpdateCostumerInputPort.java
+++ src/main/java/com/poc/hexagonal/application/ports/in/UpdateCostumerInputPort.java
@@ -2,7 +2,8 @@ package com.poc.hexagonal.application.ports.in;
 
 import com.poc.hexagonal.application.core.domain.Customer;
 
-public interface UpdateCostumerInputPort {
+public interface UpdateCustomerInputPort {
 
     void update(Customer customer, String zipCode);
+
 }
